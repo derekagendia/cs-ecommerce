d3e28ce0cbf7cfeb5fa80e3247b88743cd356598
stevy Endaman <stevyabessolo@gmail.com>
2022-02-10 21:32:01 +0100

    Update application-mark.blade.php

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,3 +1,3 @@
 <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <img src="{{ asset('assets/img/logo.png') }}" />
+    <img src="{{ asset('assets/img/logo.png') }}" style=" height:50px; width: 50px; margin-top: -30px" />
 </svg>
